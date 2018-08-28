1b2aab9f077a41ebede7b2e6cb2bfa126e10b736
Yip Bing You <leafytubistfriend@gmail.com>
2018-08-28 15:01:25 +0800

    Create folder for PS1A

---
+++ EmergencyRoom.java
@@ -0,0 +1,92 @@
+// Copy paste this Java Template and save it as "EmergencyRoom.java"
+import java.util.*;
+import java.io.*;
+
+// write your matric number here:
+// write your name here:
+// write list of collaborators here:
+// year 2018 hash code: tPW3cEr39msnZUTL2L5J (do NOT delete this line)
+
+class EmergencyRoom {
+  // if needed, declare a private data structure here that
+  // is accessible to all methods in this class
+
+  public EmergencyRoom() {
+    // Write necessary code during construction
+    //
+    // write your answer here
+
+
+
+  }
+
+  void ArriveAtHospital(String patientName, int emergencyLvl) {
+    // You have to insert the information (patientName, emergencyLvl)
+    // into your chosen data structure
+    //
+    // write your answer here
+
+
+
+  }
+
+  void UpdateEmergencyLvl(String patientName, int incEmergencyLvl) {
+    // You have to update the emergencyLvl of patientName to
+    // emergencyLvl += incEmergencyLvl
+    // and modify your chosen data structure (if needed)
+    //
+    // write your answer here
+
+
+
+  }
+
+  void Treat(String patientName) {
+    // This patientName is treated by the doctor
+    // remove him/her from your chosen data structure
+    //
+    // write your answer here
+
+
+
+  }
+
+  String Query() {
+    String ans = "The emergency suite is empty";
+
+    // You have to report the name of the patient that the doctor
+    // has to give the most attention to currently. If there is no more patient to
+    // be taken care of, return a String "The emergency suite is empty"
+    //
+    // write your answer here
+
+
+
+    return ans;
+  }
+
+  void run() throws Exception {
+    // do not alter this method
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    int numCMD = Integer.parseInt(br.readLine()); // note that numCMD is >= N
+    while (numCMD-- > 0) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int command = Integer.parseInt(st.nextToken());
+      switch (command) {
+        case 0: ArriveAtHospital(st.nextToken(), Integer.parseInt(st.nextToken())); break;
+        case 1: UpdateEmergencyLvl(st.nextToken(), Integer.parseInt(st.nextToken())); break;
+        case 2: Treat(st.nextToken()); break;
+        case 3: pr.println(Query()); break;
+      }
+    }
+    pr.close();
+  }
+
+  public static void main(String[] args) throws Exception {
+    // do not alter this method
+    EmergencyRoom ps1 = new EmergencyRoom();
+    ps1.run();
+  }
+}
