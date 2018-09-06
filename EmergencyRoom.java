348e88bc99a5122ec59dfe35e38846d343fd88b2
Yip Bing You <leafytubistfriend@gmail.com>
2018-09-07 02:28:37 +0800

    Rework to manually implement BinaryHeap (Priority Queue)
    PriorityQueue in Java not able to sort after editing key.
    Resorted to manual implementation.

--- EmergencyRoom.java
+++ EmergencyRoom.java
@@ -11,101 +11,14 @@ import java.io.*;
 class EmergencyRoom {
   // if needed, declare a private data structure here that
   // is accessible to all methods in this class
-  static int entryCounter = 0;
-
-  class Patient {
-    String patientName = "";
-    int patientPriority = 0;
-    int patientEntry = 0;
-
-
-    public Patient(String patientName, int patientPriority) {
-      this.patientName = patientName;
-      this.patientPriority = patientPriority;
-      entryCounter++;
-      this.patientEntry = entryCounter;
-
-    }
-
-    public Patient(String patientName, int patientPriority, int patientEntry) {
-      this.patientName = patientName;
-      this.patientPriority = patientPriority;
-      this.patientEntry = patientEntry;
-
-    }
-
-    void updatePatientName(String newName) {
-      this.patientName = newName;
-    }
-
-    void updatePatientPriority(int newPriority) {
-      this.patientPriority = newPriority;
-    }
-
-    String getName() {
-      return patientName;
-    }
-
-    int getPriority() {
-      return patientPriority;
-    }
-
-    int getEntry() {
-      return patientEntry;
-    }
-
-    @Override
-    public boolean equals(Object pat) {
-      if (pat == null){
-        return false;
-      }
-      final Patient other = (Patient) pat;
-      if (this.getName() != other.getName()) {
-        return false;
-      }
-      if (this.getPriority() != other.getPriority()) {
-        return false;
-      }
-      if (this.getEntry() != other.getEntry()) {
-        return false;
-      }
-      return true;
-    }
-
-    @Override
-    public int hashCode() {
-      return getName().hashCode() + 31 * getPriority() + 37 * getEntry();
-    }
-
-  }
-
-  class PatientComparator implements Comparator<Patient> {
-    @Override
-    public int compare(Patient x, Patient y) {
-      // PriorityQueue is ordinal, negating the result to turn it cardinal
-      if (x.getPriority() != y.getPriority())
-        return y.getPriority() - x.getPriority();
-      return x.getEntry() - y.getEntry();
-    }
-  }
-
-  PatientComparator patientComparator;
-  PriorityQueue patientQueue;
-  Hashtable patientHashtable;
+  BinaryHeap patientHeap;
 
   public EmergencyRoom() {
     // Write necessary code during construction
     //
     // write your answer here
+    patientHeap = new BinaryHeap();
 
-    // Declare comparator for Patient class
-    patientComparator = new PatientComparator();
-    // Declare PriorityQueue (Binary Heap) for the Queue
-    patientQueue = new PriorityQueue<Patient>(patientComparator);
-    // Declare hashtable to put in patients
-    // Key: patientName
-    // Value: Patient object
-    patientHashtable = new Hashtable();
   }
 
   void ArriveAtHospital(String patientName, int emergencyLvl) {
@@ -113,9 +26,9 @@ class EmergencyRoom {
     // into your chosen data structure
     //
     // write your answer here
-    Patient newPatient = new Patient(patientName, emergencyLvl);
-    patientQueue.add(newPatient);
-    patientHashtable.put(patientName, newPatient);
+    Patient pat = new Patient(patientName, emergencyLvl);
+    patientHeap.insert(pat);
+
   }
 
   void UpdateEmergencyLvl(String patientName, int incEmergencyLvl) {
@@ -124,15 +37,8 @@ class EmergencyRoom {
     // and modify your chosen data structure (if needed)
     //
     // write your answer here
+    patientHeap.increasePriority(patientName, incEmergencyLvl);
 
-    Patient oldPatient = (Patient) patientHashtable.get(patientName);
-    if (patientQueue.contains(oldPatient)) {
-      patientQueue.remove(oldPatient);
-      Patient updPatient = new Patient(patientName, oldPatient.getPriority() + incEmergencyLvl, oldPatient.getEntry());
-      patientQueue.add(updPatient);
-      patientHashtable.remove(patientName);
-      patientHashtable.put(patientName, updPatient);
-    }
   }
 
   void Treat(String patientName) {
@@ -140,17 +46,15 @@ class EmergencyRoom {
     // remove him/her from your chosen data structure
     //
     // write your answer here
-    /*
-    // Treat patient with highest priority
-    Patient toTreatPat = (Patient) patientQueue.poll();
-    patientHashtable.remove(toTreatPat.getName());
-    */
-    // Treat named patient
-    Patient toTreatPat = (Patient) patientHashtable.get(patientName);
-    if (patientQueue.contains(toTreatPat)) {
-      patientQueue.remove(toTreatPat);
-      patientHashtable.remove(toTreatPat);
+    if (patientHeap.peekMax().getName().equals(patientName)){
+      patientHeap.extractMax();
     }
+    else {
+      patientHeap.maxLvl(patientName);
+      patientHeap.extractMax();
+    }
+
+
   }
 
   String Query() {
@@ -161,11 +65,13 @@ class EmergencyRoom {
     // be taken care of, return a String "The emergency suite is empty"
     //
     // write your answer here
-    Patient toTreatPat = (Patient) patientQueue.peek();
-    if (toTreatPat != null) {
-      ans = toTreatPat.getName();
+    //System.out.println(patientHeap.toString());
+    if (patientHeap.isEmpty()) {
+      return ans;
     }
-    return ans;
+    else {
+      return patientHeap.peekMax().getName();
+    }
   }
 
   void run() throws Exception {
@@ -191,5 +97,202 @@ class EmergencyRoom {
     // do not alter this method
     EmergencyRoom ps1 = new EmergencyRoom();
     ps1.run();
+  }
+}
+
+class Patient {
+  String patientName = "";
+  int patientPriority = 0;
+  int patientEntry = 0;
+  static int entryCounter = 0;
+
+  public Patient(String patientName, int patientPriority, int patientEntry) {
+    this.patientName = patientName;
+    this.patientPriority = patientPriority;
+    this.patientEntry = patientEntry;
+  }
+
+  public Patient(String patientName, int patientPriority) {
+    this(patientName,patientPriority,++entryCounter);
+  }
+
+  public Patient() {
+    this("", -1);
+  }
+
+  void updatePatientName(String newName) {
+    this.patientName = newName;
+  }
+
+  void updatePatientPriority(int newPriority) {
+    this.patientPriority = newPriority;
+  }
+
+  void updatePatientEntry(int newEntry) {
+    this.patientEntry = newEntry;
+  }
+
+  String getName() {
+    return patientName;
+  }
+
+  int getPriority() {
+    return patientPriority;
+  }
+
+  int getEntry() {
+    return patientEntry;
+  }
+
+  @Override
+  public String toString() {
+    return this.getName() + "/" + this.getPriority() + "/" + this.getEntry();
+  }
+
+  @Override
+  public boolean equals(Object pat) {
+    if (pat == null){
+      return false;
+    }
+    final Patient other = (Patient) pat;
+    if (this.getName() != other.getName()) {
+      return false;
+    }
+    if (this.getPriority() != other.getPriority()) {
+      return false;
+    }
+    if (this.getEntry() != other.getEntry()) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    return getName().hashCode() + 31 * getPriority() + 37 * getEntry();
+  }
+
+  public int compareTo(Patient other) {
+    if ((this.getPriority() > other.getPriority()) || (this.getPriority() == other.getPriority() && this.getEntry() < other.getEntry()))
+      return 1;
+    return -1;
+  }
+
+}
+
+class BinaryHeap {
+  ArrayList<Patient> heapPatient;
+  HashMap<String,Patient> mapPatient;
+  HashMap<String,Integer> mapPatientIndex;
+  int heapSize;
+
+  public BinaryHeap() {
+    this.heapPatient = new ArrayList<>();
+    this.mapPatient = new HashMap<>();
+    this.mapPatientIndex = new HashMap<>();
+    this.heapSize = 0;
+
+    this.heapPatient.add(new Patient());
+  }
+
+  void insert(Patient pat) {
+    heapPatient.add(pat);
+    mapPatient.put(pat.getName(),pat);
+    heapSize++;
+    mapPatientIndex.put(pat.getName(),heapSize);
+    shiftUp(heapSize);
+  }
+
+  Patient extractMax() {
+    Patient max = heapPatient.get(1);
+    Patient last = heapPatient.get(heapSize);
+
+    heapPatient.set(1, last);
+    heapPatient.remove(heapSize--);
+
+    mapPatient.remove(max.getName());
+    mapPatientIndex.remove(max.getName());
+    mapPatientIndex.replace(last.getName(), 1);
+    shiftDown(1);
+    return max;
+  }
+
+  Patient peekMax() {
+    return heapPatient.get(1);
+  }
+
+  boolean isEmpty() {
+    return heapSize == 0;
+  }
+
+  void maxLvl(String patientName) {
+    Patient pat = mapPatient.get(patientName);
+    int pos = mapPatientIndex.get(patientName);
+    pat.updatePatientPriority(100);
+    shiftUp(pos);
+  }
+
+  void increasePriority(String patientName, int increase) {
+    Patient pat = mapPatient.get(patientName);
+    int pos = mapPatientIndex.get(patientName);
+    pat.updatePatientPriority(pat.getPriority() + increase);
+    shiftUp(pos);
+  }
+
+  void shiftUp(int i) {
+    Patient parent = heapPatient.get(parent(i));
+    Patient child = heapPatient.get(i);
+
+    while (i > 1 && parent.compareTo(child) == -1) {
+      swap(parent(i), i);
+      i = parent(i);
+      parent = heapPatient.get(parent(i));
+    }
+  }
+
+  void shiftDown(int i) {
+    while(i <= heapSize) {
+      int maxI = i;
+      Patient max = heapPatient.get(i);
+
+      if (left(i) != -1 && max.compareTo(heapPatient.get(left(i))) == -1) {
+        maxI = left(i);
+        max = heapPatient.get(left(i));
+      }
+      if (right(i) != -1 && max.compareTo(heapPatient.get(right(i))) == -1) {
+        maxI = right(i);
+        max = heapPatient.get(right(i));
+      }
+      if (maxI != i) {
+        swap(maxI, i);
+        i = maxI;
+      }
+      else break;
+    }
+  }
+
+  void swap(int a, int b) {
+    Patient patA = heapPatient.get(a);
+    Patient patB = heapPatient.get(b);
+    heapPatient.set(a, patB);
+    heapPatient.set(b, patA);
+    mapPatientIndex.replace(patA.getName(), b);
+    mapPatientIndex.replace(patB.getName(), a);
+  }
+
+  int left(int i) {
+    return 2*i > heapSize ? -1 : 2*i;
+  }
+
+  int right(int i) {
+    return 2*i + 1 > heapSize ? -1 : 2*i + 1;
+  }
+
+  int parent(int i) {
+    return i == 1 ? 1 : i/2;
+  }
+
+  public String toString() {
+    return heapPatient.toString();
   }
 }
